daaee7012b20618f870123a32b1e28b2d18480e6
park <tlsrja10@naver.com>
2019-03-26 14:13:25 +0900

    hotfix branch2

--- Variable.java
+++ Variable.java
@@ -1,7 +1,7 @@
 public class Variable{
-     public static void main(String[] arge){
-	System.out.print("hotfix hranch");
-
+	public static void main(String[] arge){
+	System.out.print("hotfix branch");
+	System.out.print("hotfix branch2");
 
      }
 }
